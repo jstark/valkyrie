32559a479174cb3cb4b08655e7ab3eaebaf815f3
John <jsstark@gmail.com>
2012-03-22 21:50:02 +0200

    python bindings (preliminary) for valkyrie

--- python/python.cpp
+++ python/python.cpp
@@ -1,14 +1,26 @@
 #include <boost/python.hpp>
+#include "valkyrie.h"
+#include "node.h"
 
-const char* test()
-{
-    return "test!";
-}
+using namespace boost::python;
+using valkyrie::Node;
 
 BOOST_PYTHON_MODULE (pyvalkyrie)
 {
-    using namespace boost::python;
-    def("test", test);
+    def("major_version", VKApiMajorVersion);
+    def("minor_version", VKApiMinorVersion);
+    def("patch_version", VKApiPatchVersion);
+    def("version", VKApiVersion);
+    def("is_at_least_version", VKApiVersionIsAtLeast);
+
+    // node
+    class_<Node>("Node", init<int, double, double, double>())
+            .def("id", &Node::get_id)
+            .def("x" , &Node::get_x)
+            .def("y" , &Node::get_y)
+            .def("z" , &Node::get_z)
+    ;
+
 }
 
 
