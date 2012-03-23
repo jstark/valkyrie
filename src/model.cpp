f25afabe7e36fca2168dd92d3c6bb5f24942a91b
John <jsstark@gmail.com>
2012-03-23 22:05:04 +0200

    valkyrie::Model::addNode

--- src/model.cpp
+++ src/model.cpp
@@ -1,10 +1,17 @@
 #include "model.h"
+#include "node.h"
 
 using valkyrie::Model;
+using valkyrie::Node;
 
 Model::Model(const std::string& modelName)
     : name_(modelName)
 {
+}
+
+bool Model::addNode(boost::shared_ptr<Node> node)
+{
+    return nodes_.insert(std::make_pair(node->get_id(), node)).second;
 }
 
 const std::string Model::get_name() const
--- src/model.h
+++ src/model.h
@@ -2,19 +2,28 @@
 #define VALKYRIE_MODEL_H_INCLUDED
 
 #include <string>
+#include <map>
+#include <iostream>
+#include "valkyrie.h"
+#include <boost/shared_ptr.hpp>
 
 namespace valkyrie
 {
 
-class Model
+class Node;
+
+class DLL_PUBLIC Model
 {
 public:
     explicit Model(const std::string& name);
+
+    bool addNode(boost::shared_ptr<Node> node);
 
     /* properties */
     const std::string get_name() const;
 private:
     const std::string name_;
+    std::map<int, boost::shared_ptr<Node> > nodes_;
 };//~Model
 
 }//~ns:valkyrie
