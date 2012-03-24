ce3f0d2e24da3040e92baf4a2a680be21d29f2ca
John <jsstark@gmail.com>
2012-03-24 23:23:28 +0200

    Model : private Entity

--- src/model.cpp
+++ src/model.cpp
@@ -4,22 +4,10 @@
 using valkyrie::Model;
 using valkyrie::Node;
 
-Model::Model(int id, const std::string& modelName)
-    : id_(id), name_(modelName)
-{
-}
-
-const int Model::get_id() const
-{
-    return id_;
-}
+Model::Model(int id, const std::string& name)
+    : Entity(id, name) {}
 
 bool Model::addNode(boost::shared_ptr<Node> node)
 {
     return nodes_.insert(std::make_pair(node->get_id(), node)).second;
-}
-
-const std::string Model::get_name() const
-{
-    return name_;
 }
--- src/model.h
+++ src/model.h
@@ -1,10 +1,13 @@
 #ifndef VALKYRIE_MODEL_H_INCLUDED
 #define VALKYRIE_MODEL_H_INCLUDED
 
+#include "valkyrie.h"
+#include "entity.h"
+
 #include <string>
 #include <map>
 #include <iostream>
-#include "valkyrie.h"
+
 #include <boost/shared_ptr.hpp>
 
 namespace valkyrie
@@ -12,19 +15,13 @@ namespace valkyrie
 
 class Node;
 
-class DLL_PUBLIC Model
+class DLL_PUBLIC Model : private Entity
 {
 public:
     Model(int id, const std::string& name);
 
     bool addNode(boost::shared_ptr<Node> node);
-
-    /* properties */
-    const int get_id() const;
-    const std::string get_name() const;
 private:
-    int id_;
-    const std::string name_;
     std::map<int, boost::shared_ptr<Node> > nodes_;
 };//~Model
 
