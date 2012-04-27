39a14a7e7c31d3c51bbd05af02fb5ebc73defc73
John <jsstark@gmail.com>
2012-04-27 14:15:47 +0300

    compiler warning fix

--- src/spc.cpp
+++ src/spc.cpp
@@ -3,7 +3,7 @@
 using valkyrie::Spc;
 
 Spc::Spc(int id, const std::string &name, int dofs, shared_ptr<Node> n)
-    : Entity(id, name), dofs_(dofs), n_(n)
+    : Entity(id, name), n_(n), dofs_(dofs)
 {
 }
 
