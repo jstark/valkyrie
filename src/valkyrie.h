cee09bba437b72f464e46434e8c5df6aa77782bf
John <jsstark@gmail.com>
2012-04-28 17:05:42 +0300

    documentation

--- src/valkyrie.h
+++ src/valkyrie.h
@@ -39,11 +39,66 @@ extern "C" {
 #define VK_API_VERSION "1.0.0"
 
 /* version related functions */
+
+/*! \fn void VKApiMajorVersion(void);
+* \brief Gets the library's major version.
+* \return returns the \ref VK_API_MAJOR value.
+* @see VKApiMinorVersion(), VKApiPatchVersion()
+*/
 extern DLL_PUBLIC int VKApiMajorVersion(void);
+
+/*! \fn void VKApiMinorVersion(void);
+* \brief Gets the library's minor version.
+* \return returns the \ref VK_API_MINOR value.
+* @see VKApiMajorVersion(), VKApiPatchVersion()
+*/
 extern DLL_PUBLIC int VKApiMinorVersion(void);
+
+/*! \fn void VKApiPatchVersion(void);
+* \brief Gets the library's patch version.
+* \return returns the \ref VK_API_PATCH value.
+* @see VKApiMajorVersion(), VKApiMinorVersion()
+*/
 extern DLL_PUBLIC int VKApiPatchVersion(void);
+
+/*! \fn void VKApiVersionIsAtLeast(int major, int minor, int patch);
+* \brief Checks the version of the Api.
+* \param major the major version.
+* \param minor the minor version.
+* \param patch the patch version.
+* \return returns whether the Api's version is at least the given version.
+* @see VKApiMajorVersion(), VKApiMinorVersion()
+*/
 extern DLL_PUBLIC int VKApiVersionIsAtLeast(int major, int minor, int patch);
+
+/*! \fn void VKApiVersion(void);
+* \brief Gets the library's Api version.
+* \return returns the \ref VK_API_VERSION value.
+* @see VKApiMajorVersion(), VKApiMinorVersion()
+*/
 extern DLL_PUBLIC const char *VKApiVersion(void);
+
+/*! \fn void VKModelCreate(int mid, const char *name);
+* \brief Creates a new FE model.
+* \param mid the model id, a positive integer.
+* \param name the model's name. If \a name is NULL, the model's name
+* will be an empty string.
+* \return TODO
+* @see
+*/
+extern int VKModelCreate(int mid, const char *name);
+
+/*! \fn void VKModelCreateNode(int mid, int nid, double x, double y, double z);
+* \brief Creates a new FE node.
+* \param mid the model's id, a positive integer.
+* \param nid the node's id, a positive integer.
+* \param x the node's x coordinate.
+* \param y the node's y coordinate.
+* \param z the node's z coordinate.
+* \return TODO
+* @see
+*/
+extern int VKModelCreateNode(int mid, int nid, double x, double y, double z);
 
 #ifdef __cplusplus
 }
